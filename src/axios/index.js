7f2dd389b03eedcf697460407d737333eb90b596
erfanmotnt <moeini.erfan@gmail.com>
2021-09-19 09:30:09 +0430

    add branch olympiad

--- src/axios/index.js
+++ src/axios/index.js
@@ -2,7 +2,7 @@ import Axios from 'axios';
 
 export const baseURL =
   process.env.NODE_ENV === 'production'
-    ? 'https://bankbackend.rastaiha.ir'
+    ? 'https://bankbackend.esfoly.ir'
     : 'http://localhost:8000'
 
 const baseAxios = Axios.create({
