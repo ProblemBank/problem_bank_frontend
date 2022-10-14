81824045fb938ef6530c1fb5c44f30dd9263542a
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2022-10-14 19:47:57 +0330

    change backend url

--- src/configs/Constants.tsx
+++ src/configs/Constants.tsx
@@ -1,4 +1,4 @@
 export const baseURL =
   process.env.NODE_ENV === 'production'
-    ? 'https://backend.problembank.ir'
+    ? 'https://problem-bank-backend.darkube.app'
     : 'http://localhost:8000';
